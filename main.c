d37161cc01c874f244e61ecb6bed767e009d5eb7
hedwig1998 <133191261+hedwig1998@users.noreply.github.com>
2023-05-30 08:15:54 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int S_chu_nhat(int c_dai, int c_rong);
+int D_chu_nhat(int d_dai, int d_rong);
+
+int main() {
+    int a, b;
+    printf("Nhap chieu dai hinh chu nhat: ");
+    scanf("%d", &a);
+    printf("Nhap chieu rong hinh chu nhat: ");
+    scanf("%d", &b);
+    S_chu_nhat(a, b);
+    D_chu_nhat(a, b);
+    return 0;
+}
+
+int S_chu_nhat(int s_dai, int s_rong){
+    int S = s_dai * s_rong;
+    printf("Dien tich hinh chu nhat la: %d\n", S);
+    return S;
+}
+
+int D_chu_nhat(int d_dai, int d_rong){
+    int D = (d_dai + d_rong) * 2;
+    printf("Chu vi hinh chu nhat la: %d\n", D);
+    return D;
+}
